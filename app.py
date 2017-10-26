679ea998970c73a14b692fa734b4a3f55c752292
Bruno Vieira <brunolsvieira@gmail.com>
2017-10-26 16:04:36 -0400

    Adding pandas

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import pandas as pd
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	template = 'index.html'
+	return render_template(template)
+
+@app.route('/maester')
+def maester():
+	data = pd.read_csv('./static/Maester Vieira - Data.csv').tail()
+	data = data.to_html(classes=['table', 'table-hover'])
+	return data
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
